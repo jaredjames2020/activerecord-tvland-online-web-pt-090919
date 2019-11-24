9ebe1ff82ce4b2d2209dcc3735ea86879e1fa349
Jared S <jaredjames2020@gmail.com>
2019-11-24 21:01:16 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :show, through: :characters
+  
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+  
+  def list_roles
+      self.characters[0].name + " - " + self.show[0].name
+  end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
   
+  def actors_list
+    self.actors.map do |list_names|
+      list_names.first_name + " " + list_names.last_name
+    end
+  end
+
 end
