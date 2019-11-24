a315364e845a8120098f2aed5612d699418d1531
Jared S <jaredjames2020@gmail.com>
2019-11-24 20:59:55 +0000

    Automatically backed up by Learn

---
+++ db/migrate/007_add_show_day_genre_season.rb
@@ -0,0 +1,7 @@
+class AddShowDayGenreSeason < ActiveRecord::Migration[5.1]
+  def change
+    add_column :show, :day, :string
+    add_column :show, :genre, :string
+    add_column :show, :season, :string
+  end
+end
