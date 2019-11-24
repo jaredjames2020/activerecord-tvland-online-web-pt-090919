2132daccf371ad51fa98c4109feb50e6749da9f0
Jared S <jaredjames2020@gmail.com>
2019-11-24 20:55:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,9 +3,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
-  
   def actors_list
-      # binding.pry
     self.actors.map do |list_names|
       list_names.first_name + " " + list_names.last_name
     end
